e68bb239e8b2f992a97dde5db1651713a313b59a
Maicol K. Oliveira <tbh.maicol.oliveira@sympla.com.br>
2024-11-04 13:57:21 -0300

    refact: adjust publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/baby_anthony/'
+  publicPath: process.env.NODE_ENV === "production" ? "/baby_anthony/" : "/",
 })
